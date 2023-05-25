282c5c9f328442ac1c8732e68f8876eda4ee7254
Smaloud <1923089319@qq.com>
2023-05-25 21:57:02 +0800

    主界面

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,12 @@ import controller.GameController;
 import controller.Level;
 import model.Chessboard;
 import view.ChessGameFrame;
+import view.ChessView.ChessStartFrame;
+
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            ChessGameFrame mainFrame = new ChessGameFrame(1100, 810);
-            GameController gameController = new GameController(mainFrame.getChessboardComponent(), new Chessboard(),Level.AI_Mid,mainFrame);
-            mainFrame.setVisible(true);
-            mainFrame.addSaveButton(gameController);
-            mainFrame.addLoadButton(gameController);
-            mainFrame.addTurns(gameController);
-            mainFrame.addRestartButton(gameController);
-            mainFrame.addBgmButton();
-        });
+        ChessStartFrame chessStartFrame = new ChessStartFrame();
     }
 }
